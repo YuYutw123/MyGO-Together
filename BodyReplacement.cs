f6e4ab4cf888b81ba150bfccf1931efa4ff3a41a
YuYu <yuyutw123@gmail.com>
2025-01-27 05:29:30 +0800

    ver 1.0.0

---
+++ BodyReplacement.cs
@@ -0,0 +1,77 @@
+﻿using ModelReplacement;
+using MydoModelMod;
+using UnityEngine;
+
+namespace MygoModelMod.Replacements
+{
+    public class AnonReplacement : BodyReplacementBase
+    {
+        protected override GameObject LoadAssetsAndReturnModel()
+        {
+            //Replace with the Asset Name from your unity project 
+            string model_name = "Anon";
+            return Assets.MainAssetBundle.LoadAsset<GameObject>(model_name);
+        }
+
+        protected override void AddModelScripts()
+        {
+            UseNoPostProcessing = true;
+        }
+    }
+    public class TomoriReplacement : BodyReplacementBase
+    {
+        protected override GameObject LoadAssetsAndReturnModel()
+        {
+            //Replace with the Asset Name from your unity project 
+            string model_name = "Tomori";
+            return Assets.MainAssetBundle.LoadAsset<GameObject>(model_name);
+        }
+
+        protected override void AddModelScripts()
+        {
+            UseNoPostProcessing = true;
+        }
+    }
+    public class RaanaReplacement : BodyReplacementBase
+    {
+        protected override GameObject LoadAssetsAndReturnModel()
+        {
+            //Replace with the Asset Name from your unity project 
+            string model_name = "Raana";
+            return Assets.MainAssetBundle.LoadAsset<GameObject>(model_name);
+        }
+
+        protected override void AddModelScripts()
+        {
+            UseNoPostProcessing = true;
+        }
+    }
+    public class SoyoReplacement : BodyReplacementBase
+    {
+        protected override GameObject LoadAssetsAndReturnModel()
+        {
+            //Replace with the Asset Name from your unity project 
+            string model_name = "Soyo";
+            return Assets.MainAssetBundle.LoadAsset<GameObject>(model_name);
+        }
+
+        protected override void AddModelScripts()
+        {
+            UseNoPostProcessing = true;
+        }
+    }
+    public class TakiReplacement : BodyReplacementBase
+    {
+        protected override GameObject LoadAssetsAndReturnModel()
+        {
+            //Replace with the Asset Name from your unity project 
+            string model_name = "Taki";
+            return Assets.MainAssetBundle.LoadAsset<GameObject>(model_name);
+        }
+
+        protected override void AddModelScripts()
+        {
+            UseNoPostProcessing = true;
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,61 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+using System.Reflection;
+using ModelReplacement;
+using BepInEx.Configuration;
+using System;
+using MygoModelMod.Replacements;
+
+namespace MydoModelMod
+{
+
+    [BepInPlugin("MygoModels", "MydoModelMod", "1.0.0")]
+    [BepInDependency("meow.ModelReplacementAPI", BepInDependency.DependencyFlags.HardDependency)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static ConfigFile config;
+
+        public void InitConfig()
+        {
+            return;
+        }
+
+        private void Awake()
+        {
+            config = base.Config;
+            InitConfig();
+            Assets.PopulateAssets();
+
+            ModelReplacementAPI.RegisterSuitModelReplacement("Anon", typeof(AnonReplacement));
+            ModelReplacementAPI.RegisterSuitModelReplacement("Tomori", typeof(TomoriReplacement));
+            ModelReplacementAPI.RegisterSuitModelReplacement("Taki", typeof(TakiReplacement));
+            ModelReplacementAPI.RegisterSuitModelReplacement("Soyo", typeof(SoyoReplacement));
+            ModelReplacementAPI.RegisterSuitModelReplacement("Raana", typeof(RaanaReplacement));
+
+
+            Harmony harmony = new Harmony("MygoModels");
+            harmony.PatchAll();
+        }
+    }
+    public static class Assets
+    {
+        // Replace mbundle with the Asset Bundle Name from your unity project 
+        public static string mainAssetBundleName = "mygobundle";
+        public static AssetBundle MainAssetBundle = null;
+
+        private static string GetAssemblyName() => Assembly.GetExecutingAssembly().GetName().Name;
+        public static void PopulateAssets()
+        {
+            if (MainAssetBundle == null)
+            {
+                Console.WriteLine(GetAssemblyName() + "." + mainAssetBundleName);
+                using (var assetStream = Assembly.GetExecutingAssembly().GetManifestResourceStream(GetAssemblyName() + "." + mainAssetBundleName))
+                {
+                    MainAssetBundle = AssetBundle.LoadFromStream(assetStream);
+                }
+
+            }
+        }
+    }
+}
